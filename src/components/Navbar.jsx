9d872d84d81a815409b1bf9a51249ab6b80b797e
dabhisharad13 <dabhisharad13@gmail.com>
2023-06-19 13:45:51 -0700

    Added Sidebar and navbar with css

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <span className="nav-logo">Fire Chat</span>
+      <div className="nav-user">
+        <img src="" alt="" />
+        <span>Sharad</span>
+        <button>Logout</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
