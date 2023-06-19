2eaed41a081e5b569aef878e37be45203579bb0e
dabhisharad13 <dabhisharad13@gmail.com>
2023-06-19 14:45:53 -0700

    Added Chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import More from "../assets/images/more.png"
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Sharad</span>
+        <div className="chatIcons">
+          <img src={More} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
