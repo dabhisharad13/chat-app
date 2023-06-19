2142f2e1d855fd104700301f628794c14bfa4ee9
dabhisharad13 <dabhisharad13@gmail.com>
2023-06-19 14:05:52 -0700

    Added Search componenet and search chat in side bar

---
+++ src/components/Search.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className="search">
+        <div className="searchForm">
+            <input type="text" placeholder='Search'/>
+        </div>
+        <div className="userChat">
+            <img src="" alt="" />
+            <div className="userChatInfo">
+                <span>Sharad</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Search
