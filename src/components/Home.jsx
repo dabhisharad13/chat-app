606c6670aa62045f70cc00a50d97130a365e6aef
dabhisharad13 <dabhisharad13@gmail.com>
2023-06-19 12:27:59 -0700

    Initialized Sidebar and Chat Component with style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <div className="container">
+        <Sidebar />
+        <Chat />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
