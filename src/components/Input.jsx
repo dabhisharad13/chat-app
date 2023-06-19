0a8c2c65d8a4fe4f82c87f2600d390a3e2747162
dabhisharad13 <dabhisharad13@gmail.com>
2023-06-19 15:18:59 -0700

    Added Input component with link and send image

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import SendImage from "../assets/images/Send.png";
+import Attach from "../assets/images/Link.png";
 
 const Input = () => {
   return (
-    <div>Input</div>
-  )
-}
+    <div className="input">
+      <input type="text" placeholder="Type something..." />
+      <div className="send">
+        <img src={Attach} alt="" />
+        <input type="file" style={{ display: "none" }} id="" file />
+        <label htmlFor="file">
+          <img src={SendImage} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
+  );
+};
 
-export default Input
+export default Input;
