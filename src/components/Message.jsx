18d50fe442bcdc7a4ccef970eeed1f0062dec502
dabhisharad13 <dabhisharad13@gmail.com>
2023-06-21 00:06:49 -0700

    Added smooth scroll when new message is sent

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,4 @@
-import React, { useContext } from "react";
-import tempImg from "../assets/images/temp.jpeg";
+import React, { useContext, useEffect, useRef } from "react";
 import { UserChatContext } from "../context/UserChat";
 import { AuthContext } from "../context/AuthContext";
 
@@ -8,9 +7,17 @@ import "../scss/message.scss";
 const Message = ({ message }) => {
   const { currentUser } = useContext(AuthContext);
   const { data } = useContext(UserChatContext);
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
 
   return (
-    <div className={`message ${message.senderId === currentUser.uid && "owner"}`}>
+    <div
+      ref={ref}
+      className={`message ${message.senderId === currentUser.uid && "owner"}`}
+    >
       <div className="messageInfo">
         <img
           src={
